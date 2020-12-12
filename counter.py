ff85a6f45fc0c37630961ce80d62cad89eb1fd30
rinnow <47298977+rinnow@users.noreply.github.com>
2020-12-12 14:06:57 +0900

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,7 +1,33 @@
 from selenium import webdriver
+import time
+import csv
 
+votes = {}
 	
 driver = webdriver.Chrome('./chromedriver')
-driver.get('https://www.asa-pro.com/koirowa/character/chara6.html')
-counter = driver.find_element_by_id('counter')
-print(counter.text)
+
+for i in range(13):
+    name=''
+    if i == 4:
+        continue
+    driver.get('https://www.asa-pro.com/koirowa/character/chara'+str(i+1)+'.html')
+    time.sleep(1)
+    counter = driver.find_element_by_id('counter').text
+    counter = int(counter)
+    char = driver.find_element_by_class_name('chara-data-set')
+    char = char.text
+    char = char[3:]
+    for j in char:
+        if j == '（':
+            break
+        else:
+            name += j
+    votes[counter]=name
+    
+#print(name)
+votes = sorted(votes.items(), reverse=True)
+#print(votes)
+with open('votes.csv', 'w') as f:
+    w = csv.writer(f, lineterminator='\n')
+    for i in votes:
+        w.writerow(i)
