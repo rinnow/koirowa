bb1f594264d3bf3de61947813aaceb12f00becb4
rinnow <47298977+rinnow@users.noreply.github.com>
2020-12-15 00:27:02 +0900

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
 import time
 import csv
+import os
+import shutil
 
+votes1 = {}
 votes = {}
+voted = {}
+array = []
 
+#chromeを開きます 適宜firefoxなどに変更できます
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
-#driver = webdriver.Chrome('./chromedriver')
 
+#driver = webdriver.Chrome('./chromedriver')
+#chrome開くのはこれだけでもOK
+
+#votes.csvが存在する倍はvoted.csvを作成
+if (os.path.exists("./votes.csv"))==True:
+    if (os.path.exists("./voted.csv"))==True:
+        os.remove("./voted.csv")
+    shutil.copyfile("./votes.csv", "./voted.csv")
+
+#各キャラクターのページを開き投票数をカウント
 for i in range(13):
     name=''
     if i == 4:
@@ -25,14 +40,35 @@ for i in range(13):
             break
         else:
             name += j
-    votes[counter]=name
+    votes1[counter]=name
+
+#投票数で降順にしてリストに入れる
+votes1 = sorted(votes1.items(), reverse=True)
+
+#もしvoted.csvがある場合今回の結果と比較
+if (os.path.exists("./voted.csv")) == True:
+    for i in votes1:
+        votes[i[0]]=i[1]
     
-#print(name)
-votes = sorted(votes.items(), reverse=True)
-#print(votes)
+    with open('./voted.csv', 'r') as f:
+        reader = csv.reader(f)
+        for i in reader:
+            voted[i[0]]=i[1]
+            
+    for i in votes.items():
+        for j in voted.items():
+            if i[1] == j[1]:
+                num = int(i[0])-int(j[0])
+                array.append([int(i[0]),i[1],'+'+str(num)])
+else:
+    array = votes1
+
+#chromeを閉じる
+driver.close()
+
+#結果を書き込み
 with open('votes.csv', 'w') as f:
     w = csv.writer(f, lineterminator='\n')
-    for i in votes:
+    for i in array:
         w.writerow(i)
 
-driver.close()
